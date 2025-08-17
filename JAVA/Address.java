7370226b2c8409257f76f2d6a8443b8b96d3092b
Tanishkaaggarwal13 <tanishka.aggarwal1310@gmail.com>
2025-08-17 15:54:27 +0530

    Create Address.java

---
+++ JAVA/Address.java
@@ -0,0 +1,11 @@
+public class address {
+
+public static void main(String [] args) {
+
+System.out.print("TANISHKA\n");
+
+System.out.println("South West Delhi");
+
+System.out.println("New Delhi"); }
+
+}
