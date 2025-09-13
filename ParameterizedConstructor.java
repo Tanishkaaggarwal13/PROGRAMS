c2193c5f61aec0395b3a8f41d1580c060763b30a
Tanishkaaggarwal13 <tanishka.aggarwal1310@gmail.com>
2025-09-13 11:21:19 +0530

    Create ParameterizedConstructor.java

---
+++ ParameterizedConstructor.java
@@ -0,0 +1,19 @@
+class Geeks {
+// data members of the class
+String name;
+int id;
+Geeks(String name, int id) {
+this.name = name;
+this.id = id;
+}
+}
+
+class GFG
+{
+public static void main(String[] args)
+{
+    Geeks geek1= new Geeks("Tanishka",19);
+System.out.println("GeekName: " + geek1.name + " and GeekId: " + geek1.id);
+
+}
+}
