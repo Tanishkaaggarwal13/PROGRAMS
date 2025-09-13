f272ff7045696613c9d3ebd4ddc519504e2b1f23
Tanishkaaggarwal13 <tanishka.aggarwal1310@gmail.com>
2025-09-13 11:36:55 +0530

    Create Geeks.java

---
+++ JAVA/Geeks.java
@@ -0,0 +1,11 @@
+class Geeks {
+    Geeks() {
+        super();
+        System.out.println("Constructor Called");
+        System.out.println("My name is Divye");
+    }
+
+    public static void main(String[] args) {
+        Geeks geek = new Geeks();
+    }
+}
